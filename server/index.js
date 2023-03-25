11670d22f046d59cbc521054f1ea4b61a15a75df
VietThang269 <114757489+VietThang269@users.noreply.github.com>
2023-03-25 15:43:50 +0700

    - Update: config base server

--- server/index.js
+++ server/index.js
@@ -8,6 +8,15 @@ const port = process.env.PORT || 8000;
 app.use(cors());
 app.use(express.json());
 
+const uri =
+  "mongodb+srv://thanhhuyenmhl:admin123@mealplan.brnruin.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(uri, { useNewUrlParser: true });
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully!");
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
